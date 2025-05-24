2583f3c327e2cc100c926421c32f94e90883afe9
vortix2950 <vortix2950@ksu.edu>
2025-05-25 00:02:57 +0300

    introduced redux

--- react_tailwind_app/src/components/Max7219Page.jsx
+++ react_tailwind_app/src/components/Max7219Page.jsx
@@ -849,9 +849,9 @@ void displayFrame(const bool matrix[8][8]) {
         <div className='flex flex-col'>
           <form class="max-w-sm mx-auto  bg-[#093710] p-4 relative">
 
-            <PinSelector label="Select Arduino Pin For DIN:" pinRef={pinDINRef} pinSetter={setPinDIN} pinhighlightSetter={setDinPinHighlight}></PinSelector>
-            <PinSelector label="Select Arduino Pin For CLK:" pinRef={pinCLKRef} pinSetter={setPinCLK} pinhighlightSetter={setClkPinHighlight}></PinSelector>
-            <PinSelector label="Select Arduino Pin For CS:" pinRef={pinCSRef} pinSetter={setPinCS} pinhighlightSetter={setCsPinHighlight}></PinSelector>
+            <PinSelector label="DIN Pin : " pinRef={pinDINRef} pinSetter={setPinDIN} pinhighlightSetter={setDinPinHighlight}></PinSelector>
+            <PinSelector label="CLK Pin : " pinRef={pinCLKRef} pinSetter={setPinCLK} pinhighlightSetter={setClkPinHighlight}></PinSelector>
+            <PinSelector label="CS Pin : " pinRef={pinCSRef} pinSetter={setPinCS} pinhighlightSetter={setCsPinHighlight}></PinSelector>
             <div
               type="button" //Needed to prevent form page refresh
 
