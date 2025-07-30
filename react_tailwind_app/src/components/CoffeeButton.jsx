4b354bcea9be5142d80220fe7a1afbf3dd49257e
vortix2950 <vortix2950@ksu.edu>
2025-07-30 18:30:20 +0300

    coffee button

--- react_tailwind_app/src/components/CoffeeButton.jsx
+++ react_tailwind_app/src/components/CoffeeButton.jsx
@@ -5,7 +5,7 @@ function CoffeeButton() {
         <div className='w-full mt-10 flex justify-center '>
 
 
-          /*  <a
+            {/* <a
                 href="https://www.buymeacoffee.com/vienspark"
                 className="block min-w-[200px] outline outline-1 outline-[#FFDD00] rounded-xl"
             >
@@ -16,8 +16,12 @@ function CoffeeButton() {
                     className="w-full h-auto"
                 />
             </a>
-			*/
-			<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="vienspark" data-color="#FFDD00" data-emoji="☕"  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+			 */}
+
+             <a href="https://www.buymeacoffee.com/vienspark" target="_blank">
+             <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" / >
+             </a>
+			
         </div>
     )
 }
